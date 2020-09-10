f8f8f01077df073bb889312734e88f4d2c533d56
lidiane <lidianegomesbarbosa@gmail.com>
2020-09-10 09:46:54 -0300

    start project

---
+++ DetailsMatterMovimentosSociais/Controller/EventosViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EventosViewController.swift
+//  DetailsMatterMovimentosSociais
+//
+//  Created by Lidiane Gomes Barbosa on 10/09/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class EventosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
