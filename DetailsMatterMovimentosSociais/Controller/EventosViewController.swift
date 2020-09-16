fd649234e4e15913fa90b5daa65ff8b962596d4a
Leonardo Fernandes <leonardo.gomes@lit.ifce.edu.br>
2020-09-16 13:07:50 -0300

    Screen layout done

--- DetailsMatterMovimentosSociais/Controller/EventosViewController.swift
+++ DetailsMatterMovimentosSociais/Controller/EventosViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class EventosViewController: UIViewController {
-
+    
+    @IBOutlet weak var eventsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        eventsTableView.delegate = self
+        eventsTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func segmentedChangedValue(_ sender: UISegmentedControl) {
+        print(sender.selectedSegmentIndex)
+    }
+    
+}
+
+extension EventosViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = eventsTableView.dequeueReusableCell(withIdentifier: "EventCell") as? EventTableViewCell
+            else { return UITableViewCell() }
+        
+        return cell
+    }
+    
 }
---
+++ DetailsMatterMovimentosSociais/EventsFiles/EventTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  EventTableViewCell.swift
+//  DetailsMatterMovimentosSociais
+//
+//  Created by Leonardo Gomes Fernandes on 16/09/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var viewCellToShadow: UIView!
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var movimentNameLabel: UILabel!
+    @IBOutlet weak var dateEventLabel: UILabel!
+    @IBOutlet weak var placeEventLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        viewCellToShadow.layer.cornerRadius = 8
+        viewCellToShadow.layer.shadowOffset = CGSize(width: 0, height: 3)
+        viewCellToShadow.layer.shadowColor = UIColor.black.cgColor
+        viewCellToShadow.layer.shadowRadius = 1
+        viewCellToShadow.layer.shadowOpacity = 0.2
+        
+        eventNameLabel.text = "GitDay"
+        movimentNameLabel.text = "Pyladies"
+        dateEventLabel.text = "20 de Setembro de 2020 - 14h"
+        placeEventLabel.text = "Centro Universitário Farias Brito - FB Uni - Fortaleza - Ce"
+        
+    }
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
