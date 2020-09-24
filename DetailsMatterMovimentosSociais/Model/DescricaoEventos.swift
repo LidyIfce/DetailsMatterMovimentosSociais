e6a9e3d5e28d3a4b117d1d87c25ed81487aaaef2
Gustavo Lemos <gsvieira08@gmail.com>
2020-09-24 17:03:29 -0300

    Criacao da tela de descricao de eventos

---
+++ DetailsMatterMovimentosSociais/Model/DescricaoEventos.swift
@@ -0,0 +1,30 @@
+//
+//  DescricaoEventos.swift
+//  DetailsMatterMovimentosSociais
+//
+//  Created by Gustavo Lemos on 17/09/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class DescricaoEventos: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
