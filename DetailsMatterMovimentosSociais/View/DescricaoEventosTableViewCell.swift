5f06a802f00a5d420499d3040f48db39c65d1256
Lidiane <lidianegomesbarbosa@gmail.com>
2020-09-29 18:56:08 -0300

    fixing assets

---
+++ DetailsMatterMovimentosSociais/View/DescricaoEventosTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  DescricaoEventosTableViewCell.swift
+//  DetailsMatterMovimentosSociais
+//
+//  Created by Lidiane Gomes Barbosa on 29/09/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
